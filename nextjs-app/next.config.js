bc4506945af3f283f1f10f37bee7f31fb2c66b23
quyk67uet <22028190@vnu.edu.vn>
2025-05-06 18:43:01 +0700

    commit

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -21,7 +21,29 @@ const nextConfig = {
       },
     ],
   },
-  output: "standalone",
+  output: 'export',
+  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', 
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers', 
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
